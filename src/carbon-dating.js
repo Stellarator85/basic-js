fadbf0c34ac30b55c114b64a19ccd2b5259e9a30
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2021-09-09 06:39:22 +0300

    fix: fix solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,20 +1,20 @@
 import { NotImplementedError } from '../extensions/index.js';
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
-
 /**
- * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
- * @return {Number | Boolean} calculated age in years or false
- * in case of incorrect sampleActivity
+ * Given matrix where you have to find cats by ears "^^"
+ *
+ * @param {Array<Array>} matrix 
+ * @return {Number} count of cats found
  *
  * @example
- * 
- * dateSample('1') => 22387
- * dateSample('WOOT!') => false
+ * countCats([
+ *  [0, 1, '^^'],
+ *  [0, '^^', 2],
+ *  ['^^', 1, 2]
+ * ]) => 3`
  *
  */
-
+export default function countCats(/* matrix */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
+}
